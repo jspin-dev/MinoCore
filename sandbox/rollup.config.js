285af16e01bf080e4cbe9e95b0fcd08ca4e00ec9
J-Spin <jspindev@gmail.com>
2023-01-28 15:44:20 -0600

    Undo

--- sandbox/rollup.config.js
+++ sandbox/rollup.config.js
@@ -58,7 +58,6 @@ export default {
 		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			preferBuiltins: false,
 			dedupe: ['svelte']
 		}),
 		commonjs(),
